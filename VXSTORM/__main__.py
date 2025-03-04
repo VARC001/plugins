73e03482c471aad6caef59fc8f8e4ff551169c70
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 01:43:51 +0530

    Create __main__.py

---
+++ VXSTORM/__main__.py
@@ -0,0 +1,33 @@
+from pyrogram import idle
+
+from VXSTORM import __version__
+from VXSTORM.core import (
+    Config,
+    ForcesubSetup,
+    GachaBotsSetup,
+    TemplateSetup,
+    UserSetup,
+    db,
+    VXSTORM,
+)
+from VXSTORM.functions.tools import initialize_git
+from VXSTORM.functions.utility import BList, Flood, TGraph
+
+
+async def main():
+    await VXSTORM.startup()
+    await db.connect()
+    await UserSetup()
+    await ForcesubSetup()
+    await GachaBotsSetup()
+    await TemplateSetup()
+    await Flood.updateFromDB()
+    await BList.updateBlacklists()
+    await TGraph.setup()
+    await initialize_git(Config.PLUGINS_REPO)
+    await VXSTORM.start_message(__version__)
+    await idle()
+
+
+if __name__ == "__main__":
+    VXSTORM.run(main())
