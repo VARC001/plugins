075fe836854b1efd30924470ee2c6f0cd769b5cf
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 03:55:38 +0530

    Create __init__.py

---
+++ VXSTORM/plugins/user/__init__.py
@@ -0,0 +1,18 @@
+from pyrogram.enums import ChatType
+
+from VXSTORM.core.clients import Pbxbot
+from VXSTORM.core.config import Config, Symbols
+from VXSTORM.core.database import db
+from VXSTORM.plugins.decorator import custom_handler, on_message
+from VXSTORM.plugins.help import HelpMenu
+
+handler = Config.HANDLERS[0]
+bot = Pbxbot.bot
+
+spam_chats = []
+
+bot_only = [ChatType.BOT]
+group_n_channel = [ChatType.GROUP, ChatType.SUPERGROUP, ChatType.CHANNEL]
+group_only = [ChatType.GROUP, ChatType.SUPERGROUP]
+private_n_bot = [ChatType.PRIVATE, ChatType.BOT]
+private_only = [ChatType.PRIVATE]
