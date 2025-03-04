fa430441d8ac3af9e814985da3da58e5d6a941c9
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 01:48:11 +0530

    Create join.py

---
+++ VXSTORM/plugins/bot/join.py
@@ -0,0 +1,75 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+import random
+from . import *
+from . import VXSTORM
+
+OPUS = list(set([  # Removes duplicate images
+    "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
+    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
+    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
+    "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
+    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
+    "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
+    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
+    "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
+    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
+    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
+    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
+    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
+    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
+    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
+    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
+    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
+    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
+    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
+    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
+    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
+    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
+    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
+    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
+    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
+    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
+    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
+    "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
+    "https://telegra.ph//file/07bb5f805178ec24871d3.jpg"
+]))
+
+#--------------------------
+
+MUST_JOIN = "STORM_TECHH"
+MUST_JOIN2 = "STORM_CORE"
+
+#------------------------
+@VXSTORM.bot.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    random.choice(OPUS),
+                    caption="ʜᴇʏ 🎧\n» ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ, ᴊᴏɪɴ ᴜꜱ ᴛᴏ ᴜꜱᴇ ᴍʏ ꜰᴇᴀᴛᴜʀᴇꜱ 🚀",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/STORM_TECHH"),
+                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/STORM_CORE"),
+                            ]
+                        ]                                            
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"⚠️ ᴘʀᴏᴍᴏᴛᴇ ᴛʜᴇ ʙᴏᴛ ᴀꜱ ᴀɴ ᴀᴅᴍɪɴ ɪɴ {MUST_JOIN} ᴛᴏ ᴇɴꜰᴏʀᴄᴇ ᴍᴜꜱᴛ-ᴊᴏɪɴ ʀᴜʟᴇꜱ.")
