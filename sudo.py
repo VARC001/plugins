48df57a4e1813e9d73f49b37b35f01ef7d5642f4
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 09:17:38 +0530

    Create sudo.py

---
+++ sudo.py
@@ -0,0 +1,15 @@
+from os import getenv
+from strings.helpers import HELPER
+from dotenv import load_dotenv
+
+OWNER_ID = "6257927828"
+
+SUDOS = getenv("SUDO_USERS", None)
+SUDO_USERS = []
+if SUDOS:
+    sudos = str(SUDOS).split(" ")
+    for sudo_id in sudos:
+        SUDO_USERS.append(int(sudo_id))
+SUDO_USERS.append(OWNER_ID)
+for x in HELPER:
+    SUDO_USERS.append(x)
