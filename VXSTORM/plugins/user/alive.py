11b77a7bf063d947f4f27301b99a63434c88c30d
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 07:40:05 +0530

    Update alive.py

--- VXSTORM/plugins/user/alive.py
+++ VXSTORM/plugins/user/alive.py
@@ -1,34 +1,41 @@
 import os
 import random
 import time
-
+import asyncio
 from pyrogram import Client
 from pyrogram.types import Message
-
+from helper.core import ReplyCheck
 from VXSTORM import START_TIME
 from VXSTORM.core import ENV
 from VXSTORM.functions.formatter import readable_time
 from VXSTORM.functions.images import generate_alive_image
 from VXSTORM.functions.templates import alive_template
-
+from helper.basic import edit_or_reply
 from . import Config, db, VXSTORM, on_message
 
-ALIVE_PIC = "https://envs.sh/Pa1.mp4"
+alive_logo = "https://envs.sh/Pa1.mp4"
 
 @on_message("alive", allow_stan=True)
-async def alive(x: Client, msg: Message):
-    bot_user = await x.get_me()
-    KEX = f"""**⧼ ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ ‌🪽 ⧽**
-➖➖➖➖➖➖➖➖➖➖➖
-**• ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ: 3.11.3**
-**• ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ: v2.0**
-**• ᴜꜱᴇʀʙᴏᴛ ᴠ-ꜱᴛᴀᴛᴜꜱ: ᴠx 2.2@ᴘ+**
-➖➖➖➖➖➖➖➖➖➖➖
-**• ᴅᴇᴠ: [Kᴜɴᴀʟ !](https://t.me/ll_KEX_ll)**
-**• ᴘᴏᴡᴇʀᴇᴅ ʙʏ Ɵᴘᴜs**
-➖➖➖➖➖➖➖➖➖➖➖
-"""
-    if ".jpg" in ALIVE_PIC or ".png" in ALIVE_PIC:
-        await x.send_photo(msg.chat.id, ALIVE_PIC, caption=KEX)
-    elif ".mp4" in ALIVE_PIC or ".MP4" in ALIVE_PIC:
-        await x.send_video(msg.chat.id, ALIVE_PIC, caption=KEX)
+async def alip(client: Client, message: Message):
+    X = await edit_or_reply(message, "✨")
+    await asyncio.sleep(1)
+    sad = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
+    uptime = await readable_time((time.time() - START_TIME))
+    man = (
+        f"<blockquote><b>⧼ ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ ‌🪽 ⧽</b></blockquote>\n"
+        f"➖➖➖➖➖➖➖➖➖➖➖\n"
+        f"<blockquote><b>• ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ: 3.11.3</b>\n<b>• ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ: v2.0</b>\n<b>• ᴜᴘᴛɪᴍᴇ:</b> {uptime}\n<b>• ᴜꜱᴇʀʙᴏᴛ ᴠ-ꜱᴛᴀᴛᴜꜱ: ᴠx 2.2@ᴘ+</b></blockquote>\n"
+        f"➖➖➖➖➖➖➖➖➖➖➖\n"
+        f"<blockquote><b>• ᴅᴇᴠ: [Kᴜɴᴀʟ !](https://t.me/ll_KEX_ll)</b>\n<b>• ᴘᴏᴡᴇʀᴇᴅ ʙʏ Ɵᴘᴜs</b></blockquote>\n"
+        f"➖➖➖➖➖➖➖➖➖➖➖"
+    )
+    try:
+      await sad(
+                message.chat.id,
+                alive_logo,
+                caption=man,
+                reply_to_message_id=ReplyCheck(message),
+            )
+      await X.delete()
+    except:
+      await X.edit(man, disable_web_page_preview=True)
