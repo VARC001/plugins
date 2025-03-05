f61e6599b1b95f5ff015e117c807e32678dedf87
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 09:52:50 +0530

    Create eval.py

---
+++ VXSTORM/plugins/user/eval.py
@@ -0,0 +1,39 @@
+import sys
+from io import StringIO
+import random
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.enums import MessageEntityType as MET, ChatAction as CA
+from pyrogram.types import Message
+from . import *
+
+
+@on_message("eval", allow_stan=True)
+def user_exec(client, message):
+    reply = message.reply_to_message
+    code = ""
+    try:
+        code = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        try:
+            code = message.text.split(" \n", maxsplit=1)[1]
+        except IndexError:
+            pass
+
+    result = sys.stdout = StringIO()
+    try:
+        exec(code)
+
+        message.edit(
+            f"<b>Code:</b>\n"
+            f"<code>{code}</code>\n\n"
+            f"<b>Result</b>:\n"
+            f"<code>{result.getvalue()}</code>"
+        )
+    except:
+        message.edit(
+            f"<b>Code:</b>\n"
+            f"<code>```python{code}```</code>\n\n"
+            f"<b>Result</b>:\n"
+            f"<code>{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}</code>"
+        )
