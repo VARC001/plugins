65c0a27670fb8dc1f2c2a4e6ef473d0c70ee3b55
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 08:34:47 +0530

    Update bspam.py

--- VXSTORM/plugins/user/bspam.py
+++ VXSTORM/plugins/user/bspam.py
@@ -11,23 +11,35 @@ async def bspam(x: Client, e: Message):
     kex = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
     if len(kex) == 2:
-        ok = await x.get_users(kex[1])
-        counts = int(kex[0])
-        for _ in range(counts):
-            reply = choice(BDAY)
-            msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
-            await x.send_message(e.chat.id, msg)
-            await asyncio.sleep(0.1)
+        try:
+            ok = await x.get_users(kex[1])
+            counts = int(kex[0])
+            await edit_or_reply(e, f"**ʙꜱᴘᴀᴍ ꜱᴛᴀʀᴛᴇᴅ**\n**ᴜꜱᴇʀ:** {ok.first_name}\n**ᴄᴏᴜɴᴛ:** {counts}")
+            
+            for _ in range(counts):
+                reply = choice(BDAY)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+        except Exception as err:
+            await edit_or_reply(e, f"**ᴇʀʀᴏʀ:** {err}")
 
     elif e.reply_to_message:
-        user_id = e.reply_to_message.from_user.id
-        ok = await x.get_users(user_id)
-        counts = int(kex[0])
-        for _ in range(counts):
-            reply = choice(BDAY)
-            msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
-            await x.send_message(e.chat.id, msg)
-            await asyncio.sleep(0.1)
+        try:
+            user_id = e.reply_to_message.from_user.id
+            ok = await x.get_users(user_id)
+            counts = int(kex[0])
+            await edit_or_reply(e, f"**ʙꜱᴘᴀᴍ ꜱᴛᴀʀᴛᴇᴅ**\n**ᴜꜱᴇʀ:** {ok.first_name}\n**ᴄᴏᴜɴᴛ:** {counts}")
+
+            for _ in range(counts):
+                reply = choice(BDAY)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+        except Exception as err:
+            await edit_or_reply(e, f"**ᴇʀʀᴏʀ:** {err}")
 
     else:
-        await edit_or_reply(".ʙꜱᴘᴀᴍ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")
+        await edit_or_reply(e, "**ᴜꜱᴀɢᴇ:** `.ʙꜱᴘᴀᴍ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>`")
