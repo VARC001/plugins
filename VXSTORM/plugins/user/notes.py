55e65df103df19ce598482325b6bc2436a28b9ff
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 09:23:04 +0530

    Create notes.py

---
+++ VXSTORM/plugins/user/notes.py
@@ -0,0 +1,27 @@
+import asyncio
+import random
+import time
+from pyrogram.types import Message
+from random import choice
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from OPUSDB.data import NOTES
+from . import *
+
+@on_message("pornspam", allow_stan=True)
+async def prns(client: Client, message: Message):
+    r = await message.reply_text("⌛")
+    quantity = message.command[1]
+    failed = 0
+    quantity = int(quantity)
+    await r.delete()
+    if int(message.chat.id) in GROUP:
+        await message.reply_text("ʏᴏᴜ ᴄᴀɴɴᴏᴛ ᴘᴏʀɴꜱᴘᴀᴍ ɪɴ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴄʜᴀᴛꜱ!")
+        return
+    for _ in range(quantity):
+        try:
+            file = random.choice(PORM)            
+            await client.send_video(chat_id=message.chat.id, video=file)
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
