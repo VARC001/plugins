f8405eabae7cffd21a7b8dc7d496a2df87e396f8
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-05 08:38:36 +0530

    Update ping.py

--- VXSTORM/plugins/user/ping.py
+++ VXSTORM/plugins/user/ping.py
@@ -9,30 +9,28 @@ from VXSTORM import START_TIME
 from VXSTORM.core import ENV
 from VXSTORM.functions.formatter import readable_time
 from VXSTORM.functions.templates import ping_template
+from VXSTORM.helper.basic import edit_or_reply
 
 from . import Config, db, VXSTORM, on_message
 
 @on_message("ping", allow_stan=True)
 async def ping(client: Client, message: Message):
     start_time = time.time()
-    x = await VXSTORM.edit(message, "⚡")
+    x = await edit_or_reply(message, "⚡")
+    
     uptime = readable_time(time.time() - START_TIME)
     img = await db.get_env(ENV.ping_pic)
     end_time = time.time()
-    speed = end_time - start_time
-    caption = await ping_template(round(speed, 3), uptime, client.me.mention)
+    speed = round(end_time - start_time, 3)
+
+    caption = await ping_template(speed, uptime, client.me.mention)
+
     if img:
-        PIC = "./VXSTORM/resources/alive.png"
         img = random.choice(img.split(" "))
         if img.endswith(".mp4"):
-            await message.reply_video(
-                img,
-                caption=caption,
-            )
+            await message.reply_video(img, caption=caption)
         else:
-            await message.reply_photo(
-                img,
-                caption=caption,
-            )
+            await message.reply_photo(img, caption=caption)
         return
-    await VXSTORM.edit(x, caption, no_link_preview=True)
+    
+    await x.edit(caption, disable_web_page_preview=True)
